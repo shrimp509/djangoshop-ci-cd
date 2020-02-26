822d35c7ccaf1d02728e290f87ce5191dc782bfe
RongSon <shrimp509@hotmail.com>
2020-02-26 18:13:24 +0800

    Update views.py: Add words for proving auto deployed

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request):
-	return HttpResponse("Hi Django on GAE")
+	return HttpResponse("Hi Django on GAE,\nI am updated when push to github,\nand deployed by CircleCI~~")
